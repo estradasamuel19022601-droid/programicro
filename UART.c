e749a03b88a57ad5b244f8e68e6938de72571a8a
estradasamuel19022601-droid <estradasamuel19022601@gmail.com>
2026-04-28 11:04:34 -0600

    Laboratorio 6
    el archivo de lab 6 es .stsln, el correcto el el que dice Main, las librerias usadas son las UART

---
+++ UART.c
@@ -0,0 +1,36 @@
+/*
+ * UART.c
+ *
+ * Created: 24/04/2026 07:27:47
+ *  Author: sames
+ */ 
+
+#include "UART.h"
+
+void initUART()
+{
+    DDRD &= ~(1<<DDD0);   // PD0 RX como entrada
+    DDRD |=  (1<<DDD1);   // PD1 TX como salida
+
+    UCSR0A = 0;
+    UCSR0B = (1<<RXCIE0)|(1<<RXEN0)|(1<<TXEN0);  // habilita RX, TX e interrupcion
+    UCSR0C = (1<<UCSZ01)|(1<<UCSZ00);             // 8 bits, async
+    UBRR0  = 103;                                  // 9600 baudios a 16MHz
+}
+
+void writeChar(char caracter)
+{
+    while(!(UCSR0A & (1<<UDRE0)));  // espera buffer libre
+    UDR0 = caracter;
+}
+
+void writeString(char *string)
+{
+    for(uint8_t i = 0; string[i] != '\0'; i++)
+    {
+        writeChar(string[i]);
+    }
+	writeChar('\r');
+	writeChar('\n');
+}
+///UART
---
+++ UART.h
@@ -0,0 +1,19 @@
+/*
+ * UART.h
+ *
+ * Created: 24/04/2026 07:28:05
+ *  Author: sames
+ */ 
+
+#ifndef UART_H_
+#define UART_H_
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+
+// Prototipos
+void initUART();
+void writeChar(char caracter);
+void writeString(char *string);
+
+#endif /* UART_H_ */
